ffaa2784e05d1abc5a7124fa893b2d7ec6d5c7c1
Guenahele <gbustin@yahoo.fr>
2022-09-26 10:09:35 +0200

    Initial Commit

---
+++ CoworkingPlaces/CoworkingPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoworkingPlacesApp.swift
+//  CoworkingPlaces
+//
+//  Created by apprenant02 on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoworkingPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
